e91a69229fcf5ec6bc9b3f51707bbe5d5f603b74
BarjaKarim <karim.barja@gmail.com>
2024-02-03 16:44:38 +0100

    yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+chez_karim = { name: "Chez Karim", address: "7 Boundary St, London E2 7JE", phone_number: "0658987485", category: "french" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", phone_number: "0668787485", category: "italian" }
+chez_orane = { name: "Chez Orane", address: "11 rue Blaise,Japon", phone_number: "0636987485", category: "japanese" }
+orval = { name: "l'orval", address: "189 rue Paul, Belgique", phone_number: "0987457485", category: "belgian" }
+dynastie = { name: "Dynastie", address: "1 rue marvie, France", phone_number: "0125478485", category: "chinese" }
+
+[chez_karim, pizza_east, chez_orane, dynastie, orval].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
